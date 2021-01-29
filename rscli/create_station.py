3cc822560fafa71e649acb0928f446d764a0ffa4
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-01-29 20:04:12 +0100

    Add WDR Stations

---
+++ rscli/create_station.py
@@ -0,0 +1,21 @@
+import uuid
+import json
+import os
+
+country = "de"
+container = "wdr"
+name = input("Station Name:")
+stream = input("Stream Url:")
+
+filename = name.lower().replace(" ", "-").replace("ü","ue").replace("ä","ae").replace("ö","oe").replace("(","").replace(")","")+".json"
+with open(os.path.join("countries", country, container, filename), "w") as file:
+    json.dump({
+        "id": str(uuid.uuid4()),
+        "name": name,
+        "imageUrl": None,
+        "streams": [{
+            "type": "mp3",
+            "rate": 128,
+            "url": stream
+        }]
+    }, file, indent=4) 
