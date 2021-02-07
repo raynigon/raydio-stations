1e8ce50aa2409bb7fd00012cbc2cded06261188b
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-02-07 11:57:30 +0100

    Update Folder Structure

--- tools/rscli/helper/stream.py
+++ tools/rscli/helper/stream.py
@@ -1,13 +1,13 @@
 from database import DatabaseRadioStream
 from typing import List
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from tempfile import NamedTemporaryFile
 import eyed3
 
 
 class StreamInfo:
 
-    def __init__(self, url: str, bit_rate: int, stream_type: str, station_name: str, station_description: str, station_url: str, station_genre: str):
+    def __init__(self, url: str, bit_rate: int, stream_type: str, station_name: str, station_description: str, station_url: str, station_genre: str, icy_metadata: bool):
         self.url = url
         self.bit_rate = bit_rate
         self.stream_type = stream_type,
@@ -15,13 +15,16 @@ class StreamInfo:
         self.station_description = station_description
         self.station_url = station_url
         self.station_genre = station_genre
+        self.icy_metadata = icy_metadata
 
 def generate_mp3_stream_info(stream: DatabaseRadioStream)->StreamInfo:
-    response = urlopen(stream.url)
+    request = Request(stream.url, headers={"Icy-MetaData": "1"})
+    response = urlopen(request)
     station_name = None
     station_description = None
     station_url = None
     station_genre = None
+    icy_meta_information = False
     for key in list(response.headers):
         if not key.startswith("icy"):
             continue
@@ -33,6 +36,7 @@ def generate_mp3_stream_info(stream: DatabaseRadioStream)->StreamInfo:
             station_url = response.headers["icy-url"]
         if key.lower() == "icy-genre":
             station_genre = response.headers["icy-genre"]
+        icy_meta_information=(key.lower() == "icy-metaint")
     with NamedTemporaryFile(suffix=".mp3") as tmp_file:
         CHUNK = 16 * 1024
         with open(tmp_file.name, 'wb') as f:
@@ -45,10 +49,19 @@ def generate_mp3_stream_info(stream: DatabaseRadioStream)->StreamInfo:
                 i += 1
         mp3_stream = eyed3.load(tmp_file.name)
         bit_rate = int(mp3_stream.info.bit_rate[1])
-    return StreamInfo(stream.url, bit_rate, "mp3", station_name, station_description, station_url, station_genre)
+    return StreamInfo(
+        stream.url, 
+        bit_rate, 
+        "mp3", 
+        station_name, 
+        station_description, 
+        station_url, 
+        station_genre,
+        icy_meta_information
+    )
 
 def validate_stream(stream: DatabaseRadioStream) -> List[str]:
-    if stream.type != "mp3":
+    if stream.stream_type != "mp3":
         return []
     try:
         stream_info = generate_mp3_stream_info(stream)
